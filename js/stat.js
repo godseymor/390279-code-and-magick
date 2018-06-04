4979f411109f9004854e4dc000b893283e4256d6
Aleksandr Seymor <godseymor@gmail.com>
2018-06-04 23:08:05 +0500

    исправляет ошибки линтера

--- js/stat.js
+++ js/stat.js
@@ -1,18 +1,18 @@
 'use strict';
 
-var CLOUD_WIDTH = 420; 
-var CLOUD_HEIGHT = 270; 
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
 var INITIAL_X = 100;
 var INITIAL_Y = 10;
 var GAP = 10;
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-window.renderStatistics = function(ctx, names, times) {
-  
+window.renderStatistics = function (ctx, names, times) {
+
   renderCloud(ctx, INITIAL_X + GAP, INITIAL_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, INITIAL_X, INITIAL_Y, '#fff');
 
@@ -42,29 +42,28 @@ window.renderStatistics = function(ctx, names, times) {
 
   ctx.fillText('Худшее время: ' + Math.floor(max) + ' ms у игрока ' + names[maxIndex], CENTERED_TIME_X, CENTERED_TIME_Y);
 
-  var BAR_CHART_HEIGHT = 150; //общая высота гистограммы
-  var SINGLE_BAR_WIDTH = 40; //ширина одной колонки
-  var BAR_INDENT = 50; //отступ между колонками
-  var FONT_GAP = 20; //отступ для текста
+  var BAR_CHART_HEIGHT = 150; //  общая высота гистограммы
+  var SINGLE_BAR_WIDTH = 40; // ширина одной колонки
+  var BAR_INDENT = 50; // отступ между колонками
+  var FONT_GAP = 20; // отступ для текста
 
-  var randomOpacity = function() { //получение рандомной прозрачности для цвета
+  var getRandomOpacity = function () { //  получение рандомной прозрачности для цвета
     return Math.ceil(Math.random() * 10) / 10;
-  }
+  };
 
   var FIRST_BAR_WIDTH = INITIAL_X + SINGLE_BAR_WIDTH;
   var FIRST_BAR_Y = CLOUD_HEIGHT - INITIAL_Y - FONT_GAP;
-  
-  for (var i = 0; i < names.length; i++) {
-    if (names[i] == 'Вы') {
+
+  for (var y = 0; y < names.length; y++) {
+    if (names[y] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      ctx.fillRect(FIRST_BAR_WIDTH + (BAR_INDENT + SINGLE_BAR_WIDTH) * i, FIRST_BAR_Y, SINGLE_BAR_WIDTH, -(times[i] * BAR_CHART_HEIGHT / max));
-      ctx.fillText(names[i], FIRST_BAR_WIDTH + (BAR_INDENT + SINGLE_BAR_WIDTH) * i, CLOUD_HEIGHT - INITIAL_Y);
-    }
-    else {
-    ctx.fillStyle = 'rgba(0, 0, 255,' + randomOpacity();')';
-    ctx.fillRect(FIRST_BAR_WIDTH + (BAR_INDENT + SINGLE_BAR_WIDTH) * i, FIRST_BAR_Y, SINGLE_BAR_WIDTH, -(times[i] * BAR_CHART_HEIGHT / max));
-    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-    ctx.fillText(names[i], FIRST_BAR_WIDTH + (BAR_INDENT + SINGLE_BAR_WIDTH) * i, CLOUD_HEIGHT - INITIAL_Y);
+      ctx.fillRect(FIRST_BAR_WIDTH + (BAR_INDENT + SINGLE_BAR_WIDTH) * y, FIRST_BAR_Y, SINGLE_BAR_WIDTH, -(times[y] * BAR_CHART_HEIGHT / max));
+      ctx.fillText(names[y], FIRST_BAR_WIDTH + (BAR_INDENT + SINGLE_BAR_WIDTH) * y, CLOUD_HEIGHT - INITIAL_Y);
+    } else {
+      ctx.fillStyle = 'rgba(0, 0, 255, ' + getRandomOpacity() + ')';
+      ctx.fillRect(FIRST_BAR_WIDTH + (BAR_INDENT + SINGLE_BAR_WIDTH) * y, FIRST_BAR_Y, SINGLE_BAR_WIDTH, -(times[y] * BAR_CHART_HEIGHT / max));
+      ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+      ctx.fillText(names[y], FIRST_BAR_WIDTH + (BAR_INDENT + SINGLE_BAR_WIDTH) * y, CLOUD_HEIGHT - INITIAL_Y);
     }
   }
-}
+};
